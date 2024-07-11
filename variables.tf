3b78c49613e1db205df21e49abf8fce8bacdefbe
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-11 16:50:54 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
-
+  variable "susbcription_id" {
+    type = string
+  }
+  variable "tenant_id" {
+    type = string
+  }
+  variable "client_id" {
+  type = string
+  }
+  variable "client_secret" {
+  type = string
+  }
+  

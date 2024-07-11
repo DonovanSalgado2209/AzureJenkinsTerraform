580d342f28673e1f641aa647ca59a24087e2ac6d
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-11 16:49:00 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,16 @@
 terraform { 
   required_provider {
     azurem = {
-      source = "hashicorp/azurem"
+      source = "hashicorp/azurerm"
       versioj = "3.93.0"
     }
   }
 }
+provider "azurerm" {
 
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features{}
+}

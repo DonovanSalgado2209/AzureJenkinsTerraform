2566fddfdf0518fd5d4165877d00f3a6551e11c7
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-16 15:23:55 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,87 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name                   = "myVnet"
+  address_space          = ["10.0.0.0/16"]
+  location               = "East US"
+  resource_group_name    = azurerm_resource_group.myterraformgroup.name
+}
+
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                    = "mySubnet"
+  resource_group_name     = azurerm_resource_group.myterraformgroup.name
+  virtual_network_name    = azurerm_virtual_network.myterraformnetwork.name
+  address_prefixes        = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                    = "mypublicIP"
+  location                = "East US"
+  resource_group_name     = azurerm_resource_group.myterraformgroup.name
+  allocation_method       = "Static"
+}
+
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                   = "myNetworkSecurityGroup"
+  location               = "East US"
+  resource_group_name    = azurerm_resouce_group.myterraformgroup.name
+
+  security_rule {
+    name                          = "SSH"
+    priority                      = 1001
+    direction                     = "Inbound"
+    access                        = "Allow"
+    protocol                      = "Tcp"
+    source_port_range             = "*"
+    destination_port_ranges       = ["22", "80", "443", "32323"]
+    source_address_prefix         = "*"
+    destination_address_prefix    = "*"
+  }
+}
+
+  resource "azurerm_network_interface" "myterraformnic" {
+    name                   = "myNIC"
+    location               = "East US"
+    resource_group_name    = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+      name                              = "myNicConfiguration"
+      subnet_id                         = azurerm_subnet.myterraformsubnet.id
+      private_ip_address_allocation     = "Dynamic"
+      public_ip_address_id              = azurerm_public_ip.myterraformpublicip.id
+    }
+  } 
+
+  resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id       = azurerm_network_interface.myterraformnic.id
+    network_security_group_id  = azurerm_network_security_group.myterraformnsg.id
+  }
+
+  resource "azurerm_linux_virtual_machine" "myterraformmvm" {
+    name                   = "AzureJenkinsTerraform"
+    location               = "East US"
+    resource_group_name    = azurerm_resource_group.myterraform.name
+    network_interface_ids  = [azurerm_network_interface.myterraformnic.id]
+    size                   = "Standard_DS1_v2"
+
+    os_disk {
+      name                 = "myOsDisk"
+      caching              = "ReadWrite"
+      storage_account_type = "StandardSSD_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer     = "UbuntuServer"
+      sku       = "18.04-LTS"
+      version   = "latest"
+    }
+
+    computer_name                    = "myvm"
+    admin_username                   = "dono"
+    admin_password                   = "Donovansalazar22"
+    disable_password_authentication  = false
+  }

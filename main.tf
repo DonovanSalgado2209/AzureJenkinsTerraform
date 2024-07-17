7c42167670c8edecd062f3100b16ba383917ebcd
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-17 17:18:32 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
   location = "East US"
-
+}
 resource "azurerm_key_vault" "example" {
   name                        = "testvault"
   location                    = azurerm_resource_group.example.location

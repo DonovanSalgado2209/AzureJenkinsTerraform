063d2d3df4db017a8c75f7c95459e2c50365cc92
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-17 17:05:57 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
   location = "East US"
 }
-
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.example.object_id
+}
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                   = "myVnet"
   address_space          = ["10.0.0.0/16"]

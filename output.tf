297561b264ef39fa5b9b8496c28de0dff574c54a
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-18 14:03:57 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "public_ip" {
   value = azurerm_linux_virtual_machine.myterraformmvm.public_ip_address
 }
+output "dns_name" {
+     value = azurerm_linux_virtual_machine.myterraformmvm.public_dns
+}

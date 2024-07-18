f5df1530df2c0cc70e169804660daa163e300173
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-17 20:37:26 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 provider "azurerm" {
-  skip_provider_registration = "true"
+  
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id

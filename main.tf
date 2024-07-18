a75e87f4bbeb122446acb992e890c7e52081e336
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-17 20:35:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
   location = "SouthIndia"
 }
+data "azurerm_client_config" "current" {
+}
 
+output "account_id" {
+  value = data.azurerm_client_config.current.client_id
+}
 
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                   = "myVnet"

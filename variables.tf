92c929b7e5295f4c7fb07eb6246ac3ad82c7f23b
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-07-17 18:07:42 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,20 +11,3 @@
   type = string
   }
   
-variable "ad_groups" {
-  description = "Azure AD groups to be added"
-  type = list(object({
-    display_name = string,
-      description  = string,
-    scope        = string,
-    role         = string
-  }))
-  default = [
-    {
-      display_name = "Group1"
-      description  = "some description",
-      scope        = "/subscriptions/xxxxx/resourcegroups/myrg" #resource group scope
-      role         = "Reader"      
-    }
-  ]
-}

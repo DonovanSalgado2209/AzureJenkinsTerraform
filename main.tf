94dd99b2f028e48f6acc177ab59d87b2a7e5e5b5
DonovanSalgado2209 <78834319+DonovanSalgado2209@users.noreply.github.com>
2024-08-08 13:35:29 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 
     computer_name                    = "DonoVM"
     admin_username                   = "d"
-    admin_password                   = "d"
+    admin_password                   = "Dono2"
     disable_password_authentication  = false
   }
 
